3c19449239fbfd42a001350d486cef5f12c1d2cf
ShayanPervez <shpe14@gmail.com>
2021-07-08 20:09:19 +0530

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function switchTheme() {
+    var elem = document.body;
+    elem.classList.toggle("dark-mode");  
+    document.getElementsByTagName("h2")[0].classList.toggle("dark-mode");
+    
+}
